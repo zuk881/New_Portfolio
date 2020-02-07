15ced3fae17517060a3b5d5c7d82e6aeb8862e6e
zuk881 <zuk881@cox.net>
2020-02-07 16:48:58 -0700

    added mobile responsive navbar and link edits

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,27 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-$(".dropdown-trigger").dropdown();
+    // Or with jQuery
+    $(document).ready(function () {
+        $('.sidenav').sidenav();
+    });
 
 
-var owl = $('.owl-carousel');
-owl.owlCarousel({
-    items:4,
-    loop:true,
-    margin:10,
-    autoplay:true,
-    autoplayTimeout:1000,
-    autoplayHoverPause:true
-});
-$('.play').on('click',function(){
-    owl.trigger('play.owl.autoplay',[1000])
-})
-$('.stop').on('click',function(){
-    owl.trigger('stop.owl.autoplay')
-})
+
+    var owl = $(".owl-carousel");
+    owl.owlCarousel({
+        items: 4,
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 1000,
+        autoplayHoverPause: true
+    });
+    // $('.play').on('click',function(){
+    //     owl.trigger('play.owl.autoplay',[1000])
+    // })
+    // $('.stop').on('click',function(){
+    //     owl.trigger('stop.owl.autoplay')
+    // })
 
 
 });
