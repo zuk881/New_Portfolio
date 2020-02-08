9fc7ccfc036ba927f3e47a2b148b36b9d402a316
zuk881 <zuk881@cox.net>
2020-02-07 21:24:45 -0700

    changed autoplay timeout

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
        loop:true,
        margin:10,
        autoplay:true,
-       autoplayTimeout:1000,
+       autoplayTimeout:2000,
        autoplayHoverPause:true,
        responsive : {
         // breakpoint from 0 up
