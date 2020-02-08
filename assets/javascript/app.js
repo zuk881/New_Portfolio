8d903d5896f513a7e8cbf8a1c26ded829ab12398
zuk881 <zuk881@cox.net>
2020-02-07 21:18:44 -0700

    added owl carousel and made it mobile responsive

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,44 @@
 $(document).ready(function () {
 
-    // Or with jQuery
+    // JQuery side-nav function call 
     $(document).ready(function () {
         $('.sidenav').sidenav();
     });
 
-
-
-    var owl = $(".owl-carousel");
-    owl.owlCarousel({
-        items: 4,
-        loop: true,
-        margin: 10,
-        autoplay: true,
-        autoplayTimeout: 1000,
-        autoplayHoverPause: true
+   // Owl carousel function
+   var owl = $('.owl-carousel');
+   owl.owlCarousel({
+       items:4,
+       loop:true,
+       margin:10,
+       autoplay:true,
+       autoplayTimeout:1000,
+       autoplayHoverPause:true,
+       responsive : {
+        // breakpoint from 0 up
+        0 : {
+            items:1
+        },
+        // breakpoint from 480 up
+        480 : {
+            itmes:2
+        },
+        // breakpoint from 768 up
+        768 : {
+            items:2
+         },
+         // breakpoint from 1000 up
+        1000 : {
+            items:4
+         }
+    }
+   
     });
-    // $('.play').on('click',function(){
-    //     owl.trigger('play.owl.autoplay',[1000])
-    // })
-    // $('.stop').on('click',function(){
-    //     owl.trigger('stop.owl.autoplay')
-    // })
-
-
+   $('.play').on('click',function(){
+       owl.trigger('play.owl.autoplay',[1000])
+   })
+   $('.stop').on('click',function(){
+       owl.trigger('stop.owl.autoplay')
+   })
+  
 });
